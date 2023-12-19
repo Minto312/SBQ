4f1779da1db6ec152abcf39f592a3368cfc0e8fb
Minto312 <karinto3219@gmail.com>
2023-12-19 06:57:12 +0000

    move images

--- janken/train.py
+++ janken/train.py
@@ -1,2 +1,19 @@
 import torch
+from .datasets import MyDatasets
 
+def pre_process():
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    torch.device(device)
+    
+    SEED = 1
+    torch.manual_seed(SEED)
+    
+    return 
+    
+
+def main():
+    pre_process()
+    
+    
+if __name__ == "__main__":
+    main()
