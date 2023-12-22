7f573cc7c7b0eb0d4d8d0d88ac4f860950a4a73d
Minto312 <karinto3219@gmail.com>
2023-12-22 07:05:10 +0000

    move

--- janken/train.py
+++ janken/train.py
@@ -1,5 +1,13 @@
+from sympy import E
 import torch
-from .datasets import MyDatasets
+from torch.utils.data import DataLoader, random_split
+from datasets import MyDatasets
+from model import Model
+import matplotlib.pyplot as plt
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
 
 def pre_process():
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -8,11 +16,20 @@ def pre_process():
     SEED = 1
     torch.manual_seed(SEED)
     
-    return 
+    SPRIT_RATIO = 0.8
+    dataset = MyDatasets(directory = "images", transform = None)
+    return torch.utils.data.random_split(dataset, [int(len(dataset) * SPRIT_RATIO), len(dataset) - int(len(dataset) * SPRIT_RATIO)], generator = torch.Generator().manual_seed(SEED))
     
 
 def main():
-    pre_process()
+    train_dataloader, val_dataloader = pre_process()
+    
+    
+        
+    
+    
+    
+    
     
     
 if __name__ == "__main__":
