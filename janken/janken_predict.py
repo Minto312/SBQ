93f32d0e4529470497de0e41e1c3b7b3529e310b
Minto312 <karinto3219@gmail.com>
2023-12-28 23:24:16 +0000

    rm ViT cause of OOM happen. use mobilenet v2

--- janken/janken_predict.py
+++ janken/janken_predict.py
@@ -4,15 +4,31 @@ from janken_train import target_size
 from janken_train import batch_size
 from janken_train import preprocessing_function
 
+import tensorflow_hub as hub
+import matplotlib.pyplot as plt
 
+
+def plot(pred_confidence, pred_class, test_gen):
+    # 最初のバッチを取得
+    images, _ = next(test_gen)
+
+    # 最初の画像を取得
+    first_image = images[0]
+    first_image = first_image.astype('float32') / 255
+
+    # 画像を表示
+    plt.imshow(first_image)
+    plt.title(f'Predicted class: {pred_class[0]}, Confidence: {pred_confidence[0][pred_class[0]]}')
+    plt.show()
+    
 def main():
     # 評価用ImageDataGenerator作成
     test_datagen = tf.keras.preprocessing.image.ImageDataGenerator(
-        preprocessing_function=preprocessing_function
+        # preprocessing_function=preprocessing_function
     )
     # 評価用ジェネレータ作成
     test_gen = test_datagen.flow_from_directory(
-        "img_test",
+        "images",
         target_size=(target_size, target_size),
         batch_size=batch_size,
         class_mode="categorical",
@@ -20,15 +36,17 @@ def main():
     )
 
     # 学習済みモデルロード
-    model = tf.keras.models.load_model("model.h5")
+    model = tf.keras.models.load_model("test_model.h5", custom_objects={'KerasLayer': hub.KerasLayer})
 
     # 予測実施
     pred_confidence = model.predict(test_gen)
     pred_class = np.argmax(pred_confidence, axis=1)
+    np.array(pred_confidence)
 
     # 予測結果ファイル出力
     print(pred_class)
     np.savetxt("result.csv", pred_class, fmt="%d")
+    plot(pred_confidence, pred_class, test_gen)
 
 
 if __name__ == "__main__":
